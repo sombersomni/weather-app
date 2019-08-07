058cc58bec3b75aa9e852e2f08858c347f85fc29
somberSomni <kartunehustla@gmail.com>
2019-08-07 01:34:02 -0500

    cleared App. starting fresh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import questions from './data/index.js';
 //components
-import Quiz from './components/Quiz.jsx';
-import StartMenu from './components/StartMenu.jsx';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faCircle } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
@@ -12,7 +9,6 @@ export default function App() {
   const [start, setStart] = useState(false);
   return (
     <div className='App'>
-      {start ? <Quiz quiz={questions} /> : <StartMenu setStart={setStart} />}
     </div>
   );
 }
